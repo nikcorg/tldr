2efbade603197fd3d817e24b67e9681566b82b19
Niklas Lindgren <niklas.lindgren@iki.fi>
2020-07-03 13:05:56 +0100

    feat: adds `amend` subcommand

--- cli/tldr/add.go
+++ cli/tldr/add.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nikcorg/tldr-cli/fetch"
 	"github.com/nikcorg/tldr-cli/input/entry"
 	"github.com/nikcorg/tldr-cli/storage"
+	"github.com/nikcorg/tldr-cli/touchable"
 	"github.com/nikcorg/tldr-cli/utils"
 
 	log "github.com/sirupsen/logrus"
@@ -17,10 +18,12 @@ var (
 )
 
 type addCmd struct {
-	interactive bool
-	url         string
-	sourceURL   string
+	interactive touchable.Bool
+	url         touchable.String
 	relatedURLs []string
+	sourceURL   touchable.String
+	title       touchable.String
+	unread      touchable.Bool
 }
 
 func (c *addCmd) Execute(subcommand string, args ...string) error {
@@ -32,14 +35,17 @@ func (c *addCmd) Execute(subcommand string, args ...string) error {
 	}
 
 	switch subcommand {
+	case "amend":
+		err = c.amendPrevious(source)
+
 	case "title":
-		err = amendTitle(source, c.url)
+		err = amendTitle(source, c.url.Val())
 
 	case "source":
-		err = amendSource(source, c.url)
+		err = amendSource(source, c.url.Val())
 
 	case "related":
-		err = amendRelated(source, c.url)
+		err = amendRelated(source, c.url.Val())
 
 	default:
 		err = c.addEntry(source)
@@ -57,45 +63,69 @@ func (c *addCmd) Execute(subcommand string, args ...string) error {
 	return nil
 }
 
+func (c *addCmd) Init() {
+	c.title = touchable.NewString("")
+	c.sourceURL = touchable.NewString("")
+	c.relatedURLs = []string{}
+	c.unread = touchable.NewBool(true)
+	c.url = touchable.NewString("")
+}
+
 func (c *addCmd) Help(subcommand string, args ...string) {
 	log.Debugf("Help for %s, %v", subcommand, args)
 }
 
 func (c *addCmd) ParseArgs(subcommand string, args ...string) error {
 	urlFound := false
-	nextArg := 0
-	for nextArg < len(args) {
-		arg := args[nextArg]
+	argsCopy := args[0:]
+
+	shift := func() {
+		argsCopy = argsCopy[1:]
+	}
+
+	for len(argsCopy) > 0 {
+		arg := argsCopy[0]
 
 		if strings.HasPrefix(arg, "-") || strings.HasPrefix(arg, "--") {
 			switch arg {
 			case "-i", "--interactive":
-				c.interactive = true
+				c.interactive.Set(true)
 
 			case "-r", "-rel", "--related":
-				nextArg++
-				url := args[nextArg]
+				shift()
+				url := argsCopy[0]
 				log.Debugf("found -r: %s", url)
-				if !strings.HasPrefix(url, "http") {
+				if url == "" {
+					return errURLNotFound
+				} else if !strings.HasPrefix(url, "http") {
 					return fmt.Errorf("%w: %s", errInvalidArg, url)
 				}
 				c.relatedURLs = append(c.relatedURLs, url)
 
 			case "-s", "--source":
-				nextArg++
-				url := args[nextArg]
+				shift()
+				url := argsCopy[0]
 				log.Debugf("found -s: %s", url)
-				if !strings.HasPrefix(url, "http") {
+				if url == "" {
+					return errURLNotFound
+				} else if !strings.HasPrefix(url, "http") {
 					return fmt.Errorf("%w: %s", errInvalidArg, url)
 				}
-				c.sourceURL = url
+				c.sourceURL.Set(url)
+
+			case "-t", "--title":
+				c.title.Set(arg)
+
+			case "-x", "--read":
+				log.Debug("setting unread=false")
+				c.unread.Set(false)
 			}
 		} else if strings.HasPrefix(arg, "http") {
 			urlFound = true
-			c.url = arg
+			c.url.Set(arg)
 		}
 
-		nextArg++
+		shift()
 	}
 
 	if !urlFound && subcommand == "" {
@@ -111,26 +141,24 @@ func (c *addCmd) addEntry(source *storage.Source) error {
 	log.Debugf("Fetching %v", c.url)
 	var res *fetch.Details
 	var err error
-	if res, err = fetch.Preview(c.url); err != nil {
-		return fmt.Errorf("Error fetching (%s): %w", c.url, err)
+	if res, err = fetch.Preview(c.url.Val()); err != nil {
+		return fmt.Errorf("Error fetching (%s): %w", c.url.Val(), err)
 	}
 	log.Debugf("Fetch result: %+v", res)
 
-	var title string = ""
-
 	if len(res.Titles) > 0 {
-		title = res.Titles[0]
+		c.title.SetUnlessTouched(res.Titles[0])
 	}
 
 	var newEntry = &storage.Entry{
 		URL:         strings.ToLower(res.URL),
-		Title:       title,
-		Unread:      true,
+		Title:       c.title.Val(),
+		Unread:      c.unread.ValOrDefault(true),
 		RelatedURLs: c.relatedURLs,
-		SourceURL:   c.sourceURL,
+		SourceURL:   c.sourceURL.Val(),
 	}
 
-	if c.interactive {
+	if c.interactive.ValOrDefault(false) {
 		entry.Create(newEntry, &entry.EditContext{Titles: res.Titles})
 	} else {
 		fmt.Printf("Title: %s\n", newEntry.Title)
@@ -180,6 +208,25 @@ func addEntryToTLDR(newEntry *storage.Entry, source *storage.Source) {
 		}, (*source.Records)...)
 		source.Records = &newRecords
 	}
+}
+
+func (c *addCmd) amendPrevious(source *storage.Source) error {
+	r := (*source.Records)[0]
+	e := &r.Entries[len(r.Entries)-1]
+
+	log.Debugf("c= %+v", c)
+
+	e.Title = c.title.ValOrDefault(e.Title)
+	e.SourceURL = c.sourceURL.ValOrDefault(e.SourceURL)
+	e.Unread = c.unread.ValOrDefault(e.Unread)
+
+	if len(c.relatedURLs) > 0 {
+		e.RelatedURLs = append(e.RelatedURLs, c.relatedURLs...)
+	}
+
+	log.Debugf("after amending: %+v", e)
+
+	return nil
 }
 
 ///
